daff919fe1e3a22d065dabab005d4126df0cb2cf
Yavuz Selim Bilgin <yavuz@bilgin.pro>
2017-08-31 17:59:35 +0300

    init project

---
+++ README.md
@@ -0,0 +1,105 @@
+# IHA News Crawler - İHA Haber Robotu
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Total Downloads][ico-downloads]][link-downloads]
+
+[EN] This package is created for crawling news from Ihlas Haber Ajansi. You have to be subscribed to IHA and obtain user credentials for being able to use this package.
+
+[TR] Bu paket İHA abonelerinin kullanıcı bilgileriyle haberleri taramaları için oluşturulmuştur. Aşağıdaki şekilde kullandığınızda son eklenen haberlerden istediğiniz adette haberi dizi olarak alabilirsiniz. Paketi kullanmak için İHA abonesi olmalı ve kullanıcı bilgilerine sahip olmalısınız.
+
+
+
+
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require bilginpro/agency-dha
+```
+
+## Usage
+
+``` php
+$crawler = new \BilginPro\Agency\Dha\Crawler([
+    'xCode' => 'your-x-parameter',
+    'yCode' => 'your-y-parameter',
+    'limit' => 10, // optional
+    'summaryLength' => 150 // optional
+]);
+
+$news = $crawler->crawl();
+```
+Calling `$crawler->crawl` will return an array like this:
+
+```php
+[{
+		"code": "737561a6b989cb305300fd60995140fe",
+		"title": "Title of the news 1",
+		"summary": "Summary...",
+		"content": "Content 1",
+		"created_at": "31.08.2017 15:56:12",
+		"category": "Genel",
+		"city": "Istanbul",
+		"images": ["http:\/\/path\/to\/news1\/image1", "http:\/\/path\/to\/news1\/image2"]
+	},
+	{
+		"code": "737561a6b989cb305300fd60995140fc",
+		"title": "Title of the news 2",
+		"summary": "Summary...",
+		"content": "Content 2",
+		"created_at": "31.08.2017 15:56:12",
+		"category": "Genel",
+		"city": "Ankara",
+		"images": ["http:\/\/path\/to\/news2\/image1", "http:\/\/path\/to\/news2\/image2"]
+	}
+]
+```
+## Change log
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Security
+
+If you discover any security related issues, please email yavuz@bilgin.pro instead of using the issue tracker.
+
+## Credits
+
+- [Yavuz Selim Bilgin][link-ysb]
+- [Murat Paksoy][link-mp]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/bilginpro/agency-dha.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/bilginpro/agency-dha/master.svg?style=flat-square
+[ico-scrutinizer]: https://img.shields.io/scrutinizer/coverage/g/bilginpro/agency-dha.svg?style=flat-square
+[ico-code-quality]: https://img.shields.io/scrutinizer/g/bilginpro/agency-dha.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/bilginpro/agency-dha.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/bilginpro/agency-dha
+[link-travis]: https://travis-ci.org/bilginpro/agency-dha
+[link-scrutinizer]: https://scrutinizer-ci.com/g/bilginpro/agency-dha/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/bilginpro/agency-dha
+[link-downloads]: https://packagist.org/packages/bilginpro/agency-dha
+[link-ysb]: https://github.com/ysb
+[link-mp]: https://github.com/slavesoul
+[link-contributors]: ../../contributors
---
+++ src/Crawler.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace BilginPro\Agency\Dha;
+
+use Carbon\Carbon;
+use GuzzleHttp;
+
+/**
+ * Class Crawler
+ * @package BilginPro\Ajans\Iha
+ */
+class Crawler
+{
+    /**
+     * @var string
+     */
+    protected $xCode = '';
+
+    /**
+     * @var string
+     */
+    protected $yCode = '';
+
+    /**
+     * @var int
+     */
+    protected $summaryLength = 150;
+
+    /**
+     * @var int
+     */
+    protected $limit = 5;
+
+    /**
+     * Create a new Crawler Instance
+     */
+    public function __construct($config)
+    {
+        $this->setParameters($config);
+    }
+
+    /**
+     * Does the magic.
+     * @return array
+     */
+    public function crawl()
+    {
+        $response = $this->fetchUrl($this->getUrl());
+        $xml = new \SimpleXMLElement($response);
+        $result = [];
+        $i = 0;
+        foreach ($xml->channel->item as $item) {
+            if ($this->limit > $i) {
+                $news = new \stdClass;
+                $news->code = (string)$item->guid;
+                $news->title = (string)$item->title;
+                $news->summary = $this->createSummary($item->description);
+                $news->content = (string)trim(preg_replace('/\s+/', ' ', $item->description)); // preg_replace is for cleaning newlines.
+                $news->created_at = (new Carbon($item->pubDate))->format('d.m.Y H:i:s');
+                $news->category = $this->titleCase(str_replace('DHA-', '', $item->category));
+                $news->city = (!empty($item->location) ? $this->titleCase($item->location) : '');
+                $news->images = [];
+                if (isset($item->photos) && count($item->photos) > 0) {
+                    foreach ($item->photos as $image) {
+                        $news->images[] = (string)$image;
+                    }
+                }
+
+                $result[] = $news;
+                $i++;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Creates short summary of the news, strip credits.
+     * @param $text
+     * @return string
+     */
+    public function createSummary($text)
+    {
+        if (strpos($text, '(DHA)') > 0) {
+            $split = explode('(DHA)', $text);
+            if (count($split) > 1) {
+                $text = $split[1];
+                $text = trim($text, ' \t\n\r\0\x0B-');
+            }
+        }
+        $summary = (string)$this->shortenString(strip_tags($text), $this->summaryLength);
+
+        return $summary;
+    }
+
+    /**
+     * Sets config parameters.
+     */
+    public function setParameters($config)
+    {
+        if (!is_array($config)) {
+            throw new \InvalidArgumentException('$config variable must be an array.');
+        }
+        if (array_key_exists('xCode', $config)) {
+            $this->xCode = $config['xCode'];
+        }
+        if (array_key_exists('yCode', $config)) {
+            $this->yCode = $config['yCode'];
+        }
+        if (array_key_exists('limit', $config)) {
+            $this->limit = $config['limit'];
+        }
+    }
+
+    /**
+     * Returns full url for crawling.
+     * @return string
+     */
+    public function getUrl()
+    {
+        $url = 'http://ajans.dha.com.tr/dhayharss_resimli.php'
+            . '?x=' . $this->xCode
+            . '&y=' . $this->yCode;
+
+        return $url;
+    }
+
+
+    /**
+     * Fethches given url and returns response as string.
+     * @param $url
+     * @param string $method
+     * @param array $options
+     *
+     * @return string
+     */
+    public function fetchUrl($url, $method = 'GET', $options = [])
+    {
+        $client = new GuzzleHttp\Client();
+        $res = $client->request($method, $url, $options);
+        if ($res->getStatusCode() == 200) {
+            return (string)$res->getBody();
+        }
+        return '';
+    }
+
+    /**
+     * Cuts the given string from the end of the appropriate word.
+     * @param $str
+     * @param $len
+     * @return string
+     */
+    public function shortenString($str, $len)
+    {
+        if (strlen($str) > $len) {
+            $str = rtrim(mb_substr($str, 0, $len, 'UTF-8'));
+            $str = substr($str, 0, strrpos($str, ' '));
+            $str .= '...';
+            $str = str_replace(',...', '...', $str);
+        }
+        return $str;
+    }
+
+    /**
+     * Converts a string to "Title Case"
+     * @param $str
+     * @return string
+     */
+    public function titleCase($str)
+    {
+        $str = mb_convert_case($str, MB_CASE_TITLE, 'UTF-8');
+        return $str;
+    }
+}
